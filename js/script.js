37f3878d80e456634cc60eec07a1f93b10ed26d3
clarice_r <claricerdallorto@gmail.com>
2021-05-07 18:21:08 +0100

    Added method to validate the user's input

--- js/script.js
+++ js/script.js
@@ -4,22 +4,41 @@ const inputLetter = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const messageRemainingGuesses = document.querySelector(".remaining");
 const numGuessesRemaining = document.querySelector(".remaining span");
-const messageToDisplay = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 
+// Shows dots as placeholder for letters in the word to guess
 const wordPlaceholder = word => {
     for (let letter of word) {
         wordInProgress.innerText += '●';
     }
 }
-
 wordPlaceholder(word);
 
+
+// Captures the input and resets it to blank
 guessButton.addEventListener("click", e => {
     e.preventDefault();
     const letter = inputLetter.value;
     console.log(letter);
     inputLetter.value = '';
-})
+    message.innerHTML = '';
+    const validInput = validateInput(letter);
+    console.log(validInput);
+});
+
+// Validate users input
+const validateInput = letter => {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (letter == "") {
+        message.innerHTML = "Please enter a letter from A to Z."
+    } else if (letter.length > 1) {
+        message.innerHTML = "Please enter only one letter at a time."
+    } else if (!acceptedLetter.test(letter)) {
+        message.innerHTML = "Only letters from A to Z are valid."
+    } else {
+        return letter;
+    }
+}
