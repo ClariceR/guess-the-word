81a45dae1dca739ba131010239e97f786d19b3b3
clarice_r <claricerdallorto@gmail.com>
2021-05-07 15:52:14 +0100

    Added global variables to select html elements

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const inputLetter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const messageRemainingGuesses = document.querySelector(".remaining");
+const numGuessesRemaining = document.querySelector(".remaining span");
+const messageToDisplay = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
